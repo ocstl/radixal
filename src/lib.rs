678e7fe5c54da98928d6c1ff8357fa8cf40e709c
Olivier Chassé St-Laurent <oliviercsl@gmail.com>
2019-09-28 17:49:51 -0400

    Add `is_palindrome` methods.
    Add doc-tests as example, remove duplicate tests.
    
    Since these methods rely on the standard library, add a "std" features flag, with a default implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Radixal provides the [`IntoDigits`](trait.IntoDigits.html) trait to simplify treating unsigned
 //! integer types as a sequence of digits under a specified radix.
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod digits_iterator;
 
@@ -10,20 +10,7 @@ use num_traits::{Unsigned, WrappingAdd, WrappingMul};
 
 /// An extension trait on unsigned integer types (`u8`, `u16`, `u32`, `u64`, `u128` and `usize`)
 /// and the corresponding `Wrapping` type.
-///
-/// # Example
-///
-/// ```
-/// use radixal::IntoDigits;
-///
-/// let mut digits = 123_u32.into_digits(10).expect("Bad radix.");
-///
-/// assert_eq!(digits.next(), Some(1));
-/// assert_eq!(digits.next(), Some(2));
-/// assert_eq!(digits.next(), Some(3));
-/// assert_eq!(digits.next(), None);
-/// ```
-pub trait IntoDigits: Copy + PartialOrd + WrappingAdd + WrappingMul + Unsigned {
+pub trait IntoDigits: Copy + PartialOrd + Ord + WrappingAdd + WrappingMul + Unsigned {
     #[doc(hidden)]
     const BINARY_RADIX: Self;
 
@@ -33,16 +20,55 @@ pub trait IntoDigits: Copy + PartialOrd + WrappingAdd + WrappingMul + Unsigned {
     /// Creates a `DigitsIterator` with a given `radix`.
     ///
     /// Returns `Err(RadixError)` if the radix is 0 or 1.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use radixal::IntoDigits;
+    ///
+    /// let mut digits = 123_u32.into_digits(10).unwrap();
+    ///
+    /// assert_eq!(digits.next(), Some(1));
+    /// assert_eq!(digits.next(), Some(2));
+    /// assert_eq!(digits.next(), Some(3));
+    /// assert_eq!(digits.next(), None);
+    /// ```
     fn into_digits(self, radix: Self) -> Result<DigitsIterator<Self>, RadixError> {
         DigitsIterator::new(self, radix)
     }
 
     /// Creates a `DigitsIterator` with a binary radix.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use radixal::IntoDigits;
+    ///
+    /// let mut digits = 12_u32.into_binary_digits();
+    ///
+    /// assert_eq!(digits.next(), Some(1));
+    /// assert_eq!(digits.next(), Some(1));
+    /// assert_eq!(digits.next(), Some(0));
+    /// assert_eq!(digits.next(), Some(0));
+    /// assert_eq!(digits.next(), None);
+    /// ```
     fn into_binary_digits(self) -> DigitsIterator<Self> {
         self.into_digits(Self::BINARY_RADIX).unwrap()
     }
 
     /// Creates a `DigitsIterator` with a decimal radix.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use radixal::IntoDigits;
+    ///
+    /// let mut digits = 12_u32.into_decimal_digits();
+    ///
+    /// assert_eq!(digits.next(), Some(1));
+    /// assert_eq!(digits.next(), Some(2));
+    /// assert_eq!(digits.next(), None);
+    /// ```
     fn into_decimal_digits(self) -> DigitsIterator<Self> {
         self.into_digits(Self::DECIMAL_RADIX).unwrap()
     }
@@ -56,19 +82,38 @@ pub trait IntoDigits: Copy + PartialOrd + WrappingAdd + WrappingMul + Unsigned {
     /// ```
     /// use radixal::IntoDigits;
     ///
-    /// let number = 123_u32;
-    /// assert_eq!(number.nbr_digits(10).unwrap(), 3);
+    /// let n = 123_u32;
+    /// assert_eq!(n.nbr_digits(10).unwrap(), 3);
     /// ```
     fn nbr_digits(self, radix: Self) -> Result<usize, RadixError> {
         self.into_digits(radix).map(DigitsIterator::count)
     }
 
     /// Counts the number of binary digits.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use radixal::IntoDigits;
+    ///
+    /// let n = 12_u32;
+    /// assert_eq!(n.nbr_binary_digits(), 4);
+    /// ```
     fn nbr_binary_digits(self) -> usize {
         self.nbr_digits(Self::BINARY_RADIX).unwrap()
     }
 
     /// Counts the number of decimal digits.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use radixal::IntoDigits;
+    ///
+    /// let n = 123_u32;
+    ///
+    /// assert_eq!(n.nbr_decimal_digits(), 3);
+    /// ```
     fn nbr_decimal_digits(self) -> usize {
         self.nbr_digits(Self::DECIMAL_RADIX).unwrap()
     }
@@ -82,10 +127,10 @@ pub trait IntoDigits: Copy + PartialOrd + WrappingAdd + WrappingMul + Unsigned {
     /// ```
     /// use radixal::IntoDigits;
     ///
-    /// let number = 123_u32;
-    /// assert!(!number.is_palindrome(10).unwrap());
-    /// let number = 121_u32;
-    /// assert!(number.is_palindrome(10).unwrap());
+    /// let n = 123_u32;
+    /// assert!(!n.is_palindrome(10).unwrap());
+    /// let n = 121_u32;
+    /// assert!(n.is_palindrome(10).unwrap());
     /// ```
     fn is_palindrome(self, radix: Self) -> Result<bool, RadixError> {
         let mut iter = self.into_digits(radix)?;
@@ -100,11 +145,33 @@ pub trait IntoDigits: Copy + PartialOrd + WrappingAdd + WrappingMul + Unsigned {
     }
 
     /// Checks if it is a palindrome under a binary number system.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use radixal::IntoDigits;
+    ///
+    /// let n = 12_u32;
+    /// assert!(!n.is_binary_palindrome());
+    /// let n = 9_u32;
+    /// assert!(n.is_binary_palindrome());
+    /// ```
     fn is_binary_palindrome(self) -> bool {
         self.is_palindrome(Self::BINARY_RADIX).unwrap()
     }
 
     /// Checks if it is a palindrome under a decimal number system.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use radixal::IntoDigits;
+    ///
+    /// let n = 123_u32;
+    /// assert!(!n.is_decimal_palindrome());
+    /// let n = 121_u32;
+    /// assert!(n.is_decimal_palindrome());
+    /// ```
     fn is_decimal_palindrome(self) -> bool {
         self.is_palindrome(Self::DECIMAL_RADIX).unwrap()
     }
@@ -121,20 +188,20 @@ pub trait IntoDigits: Copy + PartialOrd + WrappingAdd + WrappingMul + Unsigned {
     /// ```
     /// use radixal::IntoDigits;
     ///
-    /// let number = 123_u32;
-    /// let reversed = number.reverse_digits(10).unwrap();
+    /// let n = 123_u32;
+    /// let reversed = n.reverse_digits(10).unwrap();
     /// assert_eq!(reversed, 321);
     ///
     /// /// Wrapping on overflow.
-    /// let number = 255_u8;
-    /// let reversed = number.reverse_digits(10).unwrap();
-    /// assert_ne!(reversed, number);
+    /// let n = 255_u8;
+    /// let reversed = n.reverse_digits(10).unwrap();
+    /// assert_ne!(reversed, n);
     /// assert_eq!(reversed, 40);
     ///
     /// /// Trailing zeroes lead to irreversibility.
-    /// let number = 1230_u32;
-    /// let twice_reversed = number.reverse_digits(10).unwrap().reverse_digits(10).unwrap();
-    /// assert_ne!(twice_reversed, number);
+    /// let n = 1230_u32;
+    /// let twice_reversed = n.reverse_digits(10).unwrap().reverse_digits(10).unwrap();
+    /// assert_ne!(twice_reversed, n);
     /// assert_eq!(twice_reversed, 123);
     /// ```
     fn reverse_digits(self, radix: Self) -> Result<Self, RadixError> {
@@ -169,12 +236,120 @@ pub trait IntoDigits: Copy + PartialOrd + WrappingAdd + WrappingMul + Unsigned {
     /// ```
     /// use radixal::IntoDigits;
     ///
-    /// let number = 123_u32;
-    /// let reversed = number.reverse_decimal_digits();
+    /// let n = 123_u32;
+    /// let reversed = n.reverse_decimal_digits();
     /// assert_eq!(reversed, 321);
     /// ```
     fn reverse_decimal_digits(self) -> Self {
         self.reverse_digits(Self::DECIMAL_RADIX).unwrap()
+    }
+
+    /// Tests if `self` and `other` are composed of the same digits under a given radix.
+    ///
+    /// Since any number can be left-padded with `0`'s, these are ignored when doing the
+    /// comparison.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use radixal::IntoDigits;
+    ///
+    /// let n = 120_u32;
+    /// let m = 2100_u32;
+    ///
+    /// assert!(n.is_permutation(m, 10).unwrap());
+    /// ```
+    #[cfg(feature = "std")]
+    fn is_permutation(self, other: Self, radix: Self) -> Result<bool, RadixError> {
+        // This is reasonably efficient, but can be improved by short-circuiting.
+        let mut a: Vec<Self> = self.into_digits(radix)?.filter(|&n| !n.is_zero()).collect();
+        let mut b: Vec<Self> = other
+            .into_digits(radix)?
+            .filter(|&n| !n.is_zero())
+            .collect();
+
+        if a.len() != b.len() {
+            return Ok(false);
+        }
+
+        a.sort();
+        b.sort();
+        Ok(a == b)
+    }
+
+    /// Tests if `self` and `other` are composed of the same digits under a decimal radix.
+    ///
+    /// Since any number can be left-padded with `0`'s, these are ignored when doing the
+    /// comparison.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use radixal::IntoDigits;
+    ///
+    /// let n = 120_u32;
+    /// let m = 2100_u32;
+    ///
+    /// assert!(n.is_decimal_permutation(m).unwrap());
+    /// ```
+    #[cfg(feature = "std")]
+    fn is_decimal_permutation(self, other: Self) -> Result<bool, RadixError> {
+        // This is reasonably efficient, but can be improved by short-circuiting.
+        let mut a: Vec<Self> = self
+            .into_decimal_digits()
+            .filter(|&n| !n.is_zero())
+            .collect();
+        let mut b: Vec<Self> = other
+            .into_decimal_digits()
+            .filter(|&n| !n.is_zero())
+            .collect();
+
+        if a.len() != b.len() {
+            return Ok(false);
+        }
+
+        a.sort();
+        b.sort();
+        Ok(a == b)
+    }
+
+    /// Tests if `self` and `other` are composed of the same digits under a binary radix.
+    ///
+    /// Since any number can be left-padded with `0`'s, these are ignored when doing the
+    /// comparison.
+    ///
+    /// This convenience function is offered for the sake of completeness; consider using the
+    /// `count_ones` method instead.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use radixal::IntoDigits;
+    ///
+    /// let n = 12_u32;
+    /// let m = 17_u32;
+    ///
+    /// assert!(n.is_binary_permutation(m).unwrap());
+    /// ```
+    #[cfg(feature = "std")]
+    fn is_binary_permutation(self, other: Self) -> Result<bool, RadixError> {
+        // This is reasonably efficient, but can be improved by short-circuiting.
+        let mut a: Vec<Self> = self
+            .into_binary_digits()
+            .filter(|&n| !n.is_zero())
+            .collect();
+        let mut b: Vec<Self> = other
+            .into_binary_digits()
+            .filter(|&n| !n.is_zero())
+            .collect();
+
+        if a.len() != b.len() {
+            return Ok(false);
+        }
+
+        a.sort();
+        b.sort();
+        Ok(a == b)
     }
 }
 
@@ -194,73 +369,3 @@ macro_rules! impl_digits {
 }
 
 impl_digits!(u8 u16 u32 u64 u128 usize);
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn very_small() {
-        let mut digits = 8_u32.into_digits(10).unwrap();
-        assert_eq!(digits.len(), 1);
-        assert_eq!(digits.next(), Some(8));
-        assert_eq!(digits.next(), None);
-        assert_eq!(digits.next(), None);
-    }
-
-    #[test]
-    fn test_big_endian() {
-        let mut digits = 123_u32.into_digits(10).unwrap();
-
-        assert_eq!(digits.next(), Some(1));
-        assert_eq!(digits.next(), Some(2));
-        assert_eq!(digits.next(), Some(3));
-        assert_eq!(digits.next(), None);
-    }
-
-    #[test]
-    fn test_little_endian_123() {
-        let mut digits = 123_u64.into_digits(10).unwrap();
-
-        assert_eq!(digits.next_back(), Some(3));
-        assert_eq!(digits.next_back(), Some(2));
-        assert_eq!(digits.next_back(), Some(1));
-        assert_eq!(digits.next_back(), None);
-    }
-
-    #[test]
-    fn test_reversible_iterator() {
-        let mut digits = 123_u128.into_digits(10).unwrap().rev();
-
-        assert_eq!(digits.next(), Some(3));
-        assert_eq!(digits.next(), Some(2));
-        assert_eq!(digits.next(), Some(1));
-        assert_eq!(digits.next(), None);
-    }
-
-    #[test]
-    fn test_large_number_u8() {
-        let mut digits = 123_u8.into_digits(10).unwrap();
-
-        assert_eq!(digits.next(), Some(1));
-        assert_eq!(digits.next_back(), Some(3));
-        assert_eq!(digits.next(), Some(2));
-        assert_eq!(digits.next_back(), None);
-    }
-
-    #[test]
-    fn test_nbr_digits() {
-        let n = 54321_u32;
-        assert_eq!(n.nbr_digits(10), Ok(5));
-    }
-
-    #[test]
-    fn test_is_palindrome() {
-        let n = 543_212_345_u32;
-        assert!(n.is_palindrome(10).unwrap());
-        let n = 54321_u32;
-        assert!(!n.is_palindrome(10).unwrap());
-        let n = 211_u32;
-        assert!(!(n.is_palindrome(10).unwrap()));
-    }
-}
